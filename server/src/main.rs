80737b9d20fb576482d0983be8caaf61acf603b5
Banyc <36535895+Banyc@users.noreply.github.com>
2023-03-17 22:06:24 +0800

    feat: binary with dual protocols

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,16 +2,25 @@ use std::net::SocketAddr;
 
 use get_config::toml::get_config;
 use serde::Deserialize;
-use server::tcp_proxy::TcpProxy;
-use tokio::net::TcpListener;
+use server::{tcp_proxy::TcpProxy, udp_proxy::UdpProxy};
+use tokio::net::{TcpListener, UdpSocket};
 
 #[tokio::main]
 pub async fn main() {
     tracing_subscriber::fmt::init();
     let config: Config = get_config().unwrap();
-    let tcp_listener = TcpListener::bind(config.listen_addr).await.unwrap();
-    let tcp_proxy = TcpProxy::new(tcp_listener);
-    tcp_proxy.serve().await.unwrap();
+    let mut join_set = tokio::task::JoinSet::new();
+    join_set.spawn(async move {
+        let tcp_listener = TcpListener::bind(config.listen_addr).await.unwrap();
+        let tcp_proxy = TcpProxy::new(tcp_listener);
+        tcp_proxy.serve().await.unwrap();
+    });
+    join_set.spawn(async move {
+        let udp_listener = UdpSocket::bind(config.listen_addr).await.unwrap();
+        let udp_proxy = UdpProxy::new(udp_listener);
+        udp_proxy.serve().await.unwrap();
+    });
+    join_set.join_next().await.unwrap().unwrap();
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
