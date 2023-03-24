d7539bf01539d5d9bb1821d31299e3c200cef849
Banyc <36535895+Banyc@users.noreply.github.com>
2023-03-24 21:24:36 +0800

    refactor: methods

--- access/src/tcp.rs
+++ access/src/tcp.rs
@@ -23,14 +23,19 @@ impl TcpProxyAccess {
         }
     }
 
-    pub async fn build(
-        self,
-        listen_addr: impl ToSocketAddrs,
-    ) -> io::Result<TcpServer<TcpProxyAccess>> {
+    pub async fn build(self, listen_addr: impl ToSocketAddrs) -> io::Result<TcpServer<Self>> {
         let tcp_listener = tokio::net::TcpListener::bind(listen_addr)
             .await
             .inspect_err(|e| error!(?e, "Failed to bind to listen address"))?;
         Ok(TcpServer::new(tcp_listener, self))
+    }
+
+    async fn proxy(&self, downstream: &mut TcpStream) -> Result<(), ProxyProtocolError> {
+        let upstream = TcpProxyStream::establish(&self.proxy_configs, &self.destination).await?;
+        let mut upstream = upstream.into_inner();
+        tokio::io::copy_bidirectional(downstream, &mut upstream).await?;
+
+        Ok(())
     }
 }
 
@@ -38,21 +43,9 @@ impl TcpProxyAccess {
 impl TcpServerHook for TcpProxyAccess {
     #[instrument(skip(self, stream))]
     async fn handle_stream(&self, stream: &mut TcpStream) {
-        let res = proxy(stream, &self.proxy_configs, &self.destination).await;
+        let res = self.proxy(stream).await;
         if let Err(e) = res {
             error!(?e, "Failed to proxy");
         }
     }
-}
-
-async fn proxy(
-    downstream: &mut TcpStream,
-    proxy_configs: &[ProxyConfig],
-    destination: &SocketAddr,
-) -> Result<(), ProxyProtocolError> {
-    let upstream = TcpProxyStream::establish(proxy_configs, destination).await?;
-    let mut upstream = upstream.into_inner();
-    tokio::io::copy_bidirectional(downstream, &mut upstream).await?;
-
-    Ok(())
 }
--- server/src/tcp_proxy.rs
+++ server/src/tcp_proxy.rs
@@ -18,12 +18,122 @@ impl TcpProxy {
         Self { crypto }
     }
 
-    pub async fn build(self, listen_addr: impl ToSocketAddrs) -> io::Result<TcpServer<TcpProxy>> {
+    pub async fn build(self, listen_addr: impl ToSocketAddrs) -> io::Result<TcpServer<Self>> {
         let listener = TcpListener::bind(listen_addr)
             .await
             .inspect_err(|e| error!(?e, "Failed to bind to listen address"))?;
         let server = TcpServer::new(listener, self);
         Ok(server)
+    }
+
+    #[instrument(skip(self, downstream))]
+    async fn proxy(&self, downstream: &mut TcpStream) -> Result<StreamMetrics, ProxyProtocolError> {
+        let downstream_addr = downstream
+            .peer_addr()
+            .inspect_err(|e| error!(?e, "Failed to get downstream address"))?;
+        let start = std::time::Instant::now();
+
+        // Decode header
+        let header: RequestHeader = read_header_async(downstream, &self.crypto)
+            .await
+            .inspect_err(|e| error!(?e, "Failed to read header from downstream"))?;
+
+        // Prevent connections to localhost
+        if header.upstream.ip().is_loopback() {
+            error!(?header.upstream, "Refusing to connect to loopback address");
+            return Err(ProxyProtocolError::Loopback);
+        }
+
+        // Connect to upstream
+        let mut upstream = TcpStream::connect(header.upstream)
+            .await
+            .inspect_err(|e| error!(?e, ?header.upstream, "Failed to connect to upstream"))?;
+        let upstream_addr = upstream
+            .peer_addr()
+            .inspect_err(|e| error!(?e, ?header.upstream, "Failed to get upstream address"))?;
+
+        // Write Ok response
+        let resp = ResponseHeader { result: Ok(()) };
+        write_header_async(downstream, &resp, &self.crypto)
+            .await
+            .inspect_err(
+                |e| error!(?e, ?header.upstream, "Failed to write response to downstream"),
+            )?;
+
+        // Copy data
+        let (bytes_uplink, bytes_downlink) =
+            tokio::io::copy_bidirectional(downstream, &mut upstream)
+                .await
+                .inspect_err(
+                    |e| error!(?e, ?header.upstream, "Failed to copy data between streams"),
+                )?;
+
+        let end = std::time::Instant::now();
+        let metrics = StreamMetrics {
+            start,
+            end,
+            bytes_uplink,
+            bytes_downlink,
+            upstream_addr,
+            downstream_addr,
+        };
+        Ok(metrics)
+    }
+
+    #[instrument(skip(self, stream, res))]
+    async fn handle_proxy_result(
+        &self,
+        stream: &mut TcpStream,
+        res: Result<StreamMetrics, ProxyProtocolError>,
+    ) {
+        match res {
+            Ok(metrics) => info!(?metrics, "Connection closed normally"),
+            Err(e) => {
+                error!(?e, "Connection closed with error");
+                let _ = self.respond_with_error(stream, e).await.inspect_err(|e| {
+                    error!(?e, "Failed to respond with error to downstream after error")
+                });
+            }
+        }
+    }
+
+    #[instrument(skip(self, stream))]
+    async fn respond_with_error(
+        &self,
+        stream: &mut TcpStream,
+        error: ProxyProtocolError,
+    ) -> Result<(), ProxyProtocolError> {
+        let local_addr = stream
+            .local_addr()
+            .inspect_err(|e| error!(?e, "Failed to get local address"))?;
+
+        // Respond with error
+        let resp = match error {
+            ProxyProtocolError::Io(_) => ResponseHeader {
+                result: Err(ResponseError {
+                    source: local_addr,
+                    kind: ResponseErrorKind::Io,
+                }),
+            },
+            ProxyProtocolError::Bincode(_) => ResponseHeader {
+                result: Err(ResponseError {
+                    source: local_addr,
+                    kind: ResponseErrorKind::Codec,
+                }),
+            },
+            ProxyProtocolError::Loopback => ResponseHeader {
+                result: Err(ResponseError {
+                    source: local_addr,
+                    kind: ResponseErrorKind::Loopback,
+                }),
+            },
+            ProxyProtocolError::Response(err) => ResponseHeader { result: Err(err) },
+        };
+        write_header_async(stream, &resp, &self.crypto)
+            .await
+            .inspect_err(|e| error!(?e, "Failed to write response to downstream after error"))?;
+
+        Ok(())
     }
 }
 
@@ -31,8 +141,8 @@ impl TcpProxy {
 impl TcpServerHook for TcpProxy {
     #[instrument(skip(self, stream))]
     async fn handle_stream(&self, stream: &mut TcpStream) {
-        let res = proxy(stream, &self.crypto).await;
-        handle_proxy_result(stream, res, &self.crypto).await;
+        let res = self.proxy(stream).await;
+        self.handle_proxy_result(stream, res).await;
     }
 }
 
@@ -44,116 +154,6 @@ struct StreamMetrics {
     bytes_downlink: u64,
     upstream_addr: SocketAddr,
     downstream_addr: SocketAddr,
-}
-
-#[instrument(skip_all)]
-async fn proxy(
-    downstream: &mut TcpStream,
-    crypto: &XorCrypto,
-) -> Result<StreamMetrics, ProxyProtocolError> {
-    let downstream_addr = downstream
-        .peer_addr()
-        .inspect_err(|e| error!(?e, "Failed to get downstream address"))?;
-    let start = std::time::Instant::now();
-
-    // Decode header
-    let header: RequestHeader = read_header_async(downstream, crypto)
-        .await
-        .inspect_err(|e| error!(?e, "Failed to read header from downstream"))?;
-
-    // Prevent connections to localhost
-    if header.upstream.ip().is_loopback() {
-        error!(?header.upstream, "Refusing to connect to loopback address");
-        return Err(ProxyProtocolError::Loopback);
-    }
-
-    // Connect to upstream
-    let mut upstream = TcpStream::connect(header.upstream)
-        .await
-        .inspect_err(|e| error!(?e, ?header.upstream, "Failed to connect to upstream"))?;
-    let upstream_addr = upstream
-        .peer_addr()
-        .inspect_err(|e| error!(?e, ?header.upstream, "Failed to get upstream address"))?;
-
-    // Write Ok response
-    let resp = ResponseHeader { result: Ok(()) };
-    write_header_async(downstream, &resp, crypto)
-        .await
-        .inspect_err(|e| error!(?e, ?header.upstream, "Failed to write response to downstream"))?;
-
-    // Copy data
-    let (bytes_uplink, bytes_downlink) = tokio::io::copy_bidirectional(downstream, &mut upstream)
-        .await
-        .inspect_err(|e| error!(?e, ?header.upstream, "Failed to copy data between streams"))?;
-
-    let end = std::time::Instant::now();
-    let metrics = StreamMetrics {
-        start,
-        end,
-        bytes_uplink,
-        bytes_downlink,
-        upstream_addr,
-        downstream_addr,
-    };
-    Ok(metrics)
-}
-
-#[instrument(skip(stream, res, crypto))]
-async fn handle_proxy_result(
-    stream: &mut TcpStream,
-    res: Result<StreamMetrics, ProxyProtocolError>,
-    crypto: &XorCrypto,
-) {
-    match res {
-        Ok(metrics) => info!(?metrics, "Connection closed normally"),
-        Err(e) => {
-            error!(?e, "Connection closed with error");
-            let _ = respond_with_error(stream, e, crypto)
-                .await
-                .inspect_err(|e| {
-                    error!(?e, "Failed to respond with error to downstream after error")
-                });
-        }
-    }
-}
-
-#[instrument(skip(stream, crypto))]
-async fn respond_with_error(
-    stream: &mut TcpStream,
-    error: ProxyProtocolError,
-    crypto: &XorCrypto,
-) -> Result<(), ProxyProtocolError> {
-    let local_addr = stream
-        .local_addr()
-        .inspect_err(|e| error!(?e, "Failed to get local address"))?;
-
-    // Respond with error
-    let resp = match error {
-        ProxyProtocolError::Io(_) => ResponseHeader {
-            result: Err(ResponseError {
-                source: local_addr,
-                kind: ResponseErrorKind::Io,
-            }),
-        },
-        ProxyProtocolError::Bincode(_) => ResponseHeader {
-            result: Err(ResponseError {
-                source: local_addr,
-                kind: ResponseErrorKind::Codec,
-            }),
-        },
-        ProxyProtocolError::Loopback => ResponseHeader {
-            result: Err(ResponseError {
-                source: local_addr,
-                kind: ResponseErrorKind::Loopback,
-            }),
-        },
-        ProxyProtocolError::Response(err) => ResponseHeader { result: Err(err) },
-    };
-    write_header_async(stream, &resp, crypto)
-        .await
-        .inspect_err(|e| error!(?e, "Failed to write response to downstream after error"))?;
-
-    Ok(())
 }
 
 #[cfg(test)]
