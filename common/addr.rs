145a885535fff22e4d5ff83db5f4407e7a7bcad5
Banyc <36535895+Banyc@users.noreply.github.com>
2023-03-24 22:32:19 +0800

    style: predefined addr

--- common/addr.rs
+++ common/addr.rs
@@ -1,9 +1,9 @@
-use std::net::{IpAddr, SocketAddr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
 
 pub fn any_addr(ip_version: &IpAddr) -> SocketAddr {
     let any_ip = match ip_version {
-        IpAddr::V4(_) => IpAddr::V4("0.0.0.0".parse().unwrap()),
-        IpAddr::V6(_) => IpAddr::V6("::".parse().unwrap()),
+        IpAddr::V4(_) => Ipv4Addr::UNSPECIFIED.into(),
+        IpAddr::V6(_) => Ipv6Addr::UNSPECIFIED.into(),
     };
     SocketAddr::new(any_ip, 0)
 }
