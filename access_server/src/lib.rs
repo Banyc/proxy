8b192213a2c597e20174fd06620702e3a3736dd3
Banyc <36535895+Banyc@users.noreply.github.com>
2023-05-29 01:22:45 +0800

    feat: singular form for toml table names

--- access_server/src/lib.rs
+++ access_server/src/lib.rs
@@ -12,35 +12,35 @@ pub mod udp;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AccessServerSpawner {
-    pub tcp_servers: Option<Vec<TcpProxyAccessBuilder>>,
-    pub udp_servers: Option<Vec<UdpProxyAccessBuilder>>,
-    pub http_servers: Option<Vec<HttpProxyAccessBuilder>>,
+    pub tcp_server: Option<Vec<TcpProxyAccessBuilder>>,
+    pub udp_server: Option<Vec<UdpProxyAccessBuilder>>,
+    pub http_server: Option<Vec<HttpProxyAccessBuilder>>,
 }
 
 impl AccessServerSpawner {
     pub async fn spawn(self, join_set: &mut tokio::task::JoinSet<AnyResult>) {
-        if let Some(tcp_servers) = self.tcp_servers {
-            for tcp_server in tcp_servers {
+        if let Some(tcp_server) = self.tcp_server {
+            for server in tcp_server {
                 join_set.spawn(async move {
-                    let server = tcp_server.build().await?;
+                    let server = server.build().await?;
                     server.serve().await?;
                     Ok(())
                 });
             }
         }
-        if let Some(udp_servers) = self.udp_servers {
-            for udp_server in udp_servers {
+        if let Some(udp_server) = self.udp_server {
+            for server in udp_server {
                 join_set.spawn(async move {
-                    let server = udp_server.build().await?;
+                    let server = server.build().await?;
                     server.serve().await?;
                     Ok(())
                 });
             }
         }
-        if let Some(http_servers) = self.http_servers {
-            for http_server in http_servers {
+        if let Some(http_server) = self.http_server {
+            for server in http_server {
                 join_set.spawn(async move {
-                    let server = http_server.build().await?;
+                    let server = server.build().await?;
                     server.serve().await?;
                     Ok(())
                 });
--- proxy_server/src/lib.rs
+++ proxy_server/src/lib.rs
@@ -10,35 +10,35 @@ pub mod udp;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
 pub struct ProxyServerSpawner {
-    pub tcp_servers: Option<Vec<TcpProxyServerBuilder>>,
-    pub udp_servers: Option<Vec<UdpProxyServerBuilder>>,
-    pub kcp_servers: Option<Vec<KcpProxyServerBuilder>>,
+    pub tcp_server: Option<Vec<TcpProxyServerBuilder>>,
+    pub udp_server: Option<Vec<UdpProxyServerBuilder>>,
+    pub kcp_server: Option<Vec<KcpProxyServerBuilder>>,
 }
 
 impl ProxyServerSpawner {
     pub async fn spawn(self, join_set: &mut tokio::task::JoinSet<AnyResult>) {
-        if let Some(tcp_servers) = self.tcp_servers {
-            for tcp_server in tcp_servers {
+        if let Some(tcp_server) = self.tcp_server {
+            for server in tcp_server {
                 join_set.spawn(async move {
-                    let server = tcp_server.build().await?;
+                    let server = server.build().await?;
                     server.serve().await?;
                     Ok(())
                 });
             }
         }
-        if let Some(udp_servers) = self.udp_servers {
-            for udp_server in udp_servers {
+        if let Some(udp_server) = self.udp_server {
+            for server in udp_server {
                 join_set.spawn(async move {
-                    let server = udp_server.build().await?;
+                    let server = server.build().await?;
                     server.serve().await?;
                     Ok(())
                 });
             }
         }
-        if let Some(kcp_servers) = self.kcp_servers {
-            for kcp_server in kcp_servers {
+        if let Some(kcp_server) = self.kcp_server {
+            for server in kcp_server {
                 join_set.spawn(async move {
-                    let server = kcp_server.build().await?;
+                    let server = server.build().await?;
                     server.serve().await?;
                     Ok(())
                 });
