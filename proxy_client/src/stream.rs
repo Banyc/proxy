a5daea3140e7cf3b486f8d69839464a8befa6751
Banyc <36535895+Banyc@users.noreply.github.com>
2023-06-19 23:32:48 +0800

    feat: stream tracer

--- proxy_client/src/stream.rs
+++ proxy_client/src/stream.rs
@@ -1,11 +1,15 @@
-use std::{net::SocketAddr, time::Duration};
+use std::{
+    net::SocketAddr,
+    time::{Duration, Instant},
+};
 
 use common::{
     config::convert_proxies_to_header_crypto_pairs,
     crypto::XorCryptoCursor,
     header::{
-        codec::{timed_write_header_async, CodecError},
+        codec::{timed_read_header_async, timed_write_header_async, CodecError},
         heartbeat::{self, HeartbeatError},
+        route::{RouteError, RouteResponse},
     },
     stream::{
         addr::StreamAddr, config::StreamProxyConfig, connect_with_pool, pool::Pool, ConnectError,
@@ -103,4 +107,57 @@ pub enum StreamEstablishError {
         source: CodecError,
         upstream_addr: StreamAddr,
     },
+}
+
+pub async fn trace_rtt(
+    proxies: &[StreamProxyConfig],
+    stream_pool: &Pool,
+) -> Result<Duration, TraceError> {
+    if proxies.is_empty() {
+        return Ok(Duration::from_secs(0));
+    }
+
+    // Connect to the first proxy
+    let (mut stream, _addr, _sock_addr) = {
+        let proxy_addr = &proxies[0].address;
+        let (stream, sock_addr) =
+            connect_with_pool(proxy_addr, stream_pool, true, IO_TIMEOUT).await?;
+        (stream, proxy_addr.clone(), sock_addr)
+    };
+
+    // Convert addresses to headers
+    let pairs = convert_proxies_to_header_crypto_pairs(proxies, None);
+
+    let start = Instant::now();
+
+    // Write headers to stream
+    for (header, crypto) in pairs.as_ref() {
+        heartbeat::send_upgrade(&mut stream, IO_TIMEOUT).await?;
+        let mut crypto_cursor = XorCryptoCursor::new(crypto);
+        timed_write_header_async(&mut stream, &header, &mut crypto_cursor, IO_TIMEOUT).await?;
+    }
+
+    // Read response
+    let mut crypto_cursor = XorCryptoCursor::new(pairs.last().unwrap().1);
+    let resp: RouteResponse =
+        timed_read_header_async(&mut stream, &mut crypto_cursor, IO_TIMEOUT).await?;
+    if let Err(err) = resp.result {
+        return Err(TraceError::Response { err });
+    }
+
+    let end = Instant::now();
+
+    Ok(end.duration_since(start))
+}
+
+#[derive(Debug, Error)]
+pub enum TraceError {
+    #[error("Connect error")]
+    ConnectError(#[from] ConnectError),
+    #[error("Heartbeat error")]
+    HeartbeatError(#[from] HeartbeatError),
+    #[error("Codec error")]
+    Header(#[from] CodecError),
+    #[error("Upstream responded with an error")]
+    Response { err: RouteError },
 }
