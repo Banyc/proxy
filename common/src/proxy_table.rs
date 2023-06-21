7c7f9d496585371c4b71be7ffc463ac334b996dd
Banyc <36535895+Banyc@users.noreply.github.com>
2023-06-21 19:19:19 +0800

    feat: gauge trace loss

--- common/src/proxy_table.rs
+++ common/src/proxy_table.rs
@@ -6,11 +6,14 @@ use std::{
 use async_trait::async_trait;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
-use tracing::{info, warn};
+use tracing::{info, trace};
 
 use crate::{crypto::XorCrypto, error::AnyError, header::route::RouteRequest};
 
 const TRACE_INTERVAL: Duration = Duration::from_secs(30);
+const TRACE_DEAD_INTERVAL: Duration = Duration::from_secs(60 * 60);
+const TRACES_PER_WAVE: usize = 60;
+const TRACE_BURST_GAP: Duration = Duration::from_millis(10);
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
 pub struct ProxyConfig<A> {
@@ -95,6 +98,7 @@ pub struct WeightedProxyChain<A> {
 struct GaugedProxyChain<A> {
     weighted: WeightedProxyChain<A>,
     rtt: Arc<RwLock<Option<Duration>>>,
+    loss: Arc<RwLock<Option<f64>>>,
     task_handle: Option<tokio::task::JoinHandle<()>>,
 }
 
@@ -108,28 +112,63 @@ where
     {
         let chain = weighted.chain.clone();
         let rtt = Arc::new(RwLock::new(None));
+        let loss = Arc::new(RwLock::new(None));
         let rtt_clone = rtt.clone();
+        let loss_clone = loss.clone();
         let task_handle = tracer.map(|tracer| {
             tokio::task::spawn(async move {
+                let mut wave = tokio::task::JoinSet::new();
                 loop {
-                    let rtt = tracer.trace_rtt(&chain).await;
-                    match rtt {
-                        Ok(rtt) => {
-                            let addresses =
-                                chain.iter().map(|c| c.address.clone()).collect::<Vec<_>>();
-                            info!(?addresses, ?rtt, "Traced RTT");
-                            let mut rtt_clone = rtt_clone.write().unwrap();
-                            *rtt_clone = Some(rtt);
-                        }
-                        Err(e) => warn!("{:?}", e),
+                    // Spawn tracing tasks
+                    for _ in 0..TRACES_PER_WAVE {
+                        let chain = chain.clone();
+                        let tracer = tracer.clone();
+                        wave.spawn(async move { tracer.trace_rtt(&chain).await });
+                        tokio::time::sleep(TRACE_BURST_GAP).await;
                     }
-                    tokio::time::sleep(TRACE_INTERVAL).await;
+
+                    // Collect RTT
+                    let mut rtt_sum = Duration::from_secs(0);
+                    let mut rtt_count: usize = 0;
+                    while let Some(res) = wave.join_next().await {
+                        match res.unwrap() {
+                            Ok(rtt) => {
+                                rtt_sum += rtt;
+                                rtt_count += 1;
+                            }
+                            Err(e) => {
+                                trace!("{:?}", e);
+                            }
+                        }
+                    }
+                    let rtt = rtt_sum / (rtt_count as u32);
+                    let loss = (TRACES_PER_WAVE - rtt_count) as f64 / TRACES_PER_WAVE as f64;
+
+                    // Store RTT
+                    let addresses = chain.iter().map(|c| c.address.clone()).collect::<Vec<_>>();
+                    info!(?addresses, ?rtt, ?loss, "Traced RTT");
+                    {
+                        let mut rtt_clone = rtt_clone.write().unwrap();
+                        *rtt_clone = Some(rtt);
+                    }
+                    {
+                        let mut loss_clone = loss_clone.write().unwrap();
+                        *loss_clone = Some(loss);
+                    }
+
+                    // Sleep
+                    if rtt_count == 0 {
+                        tokio::time::sleep(TRACE_DEAD_INTERVAL).await;
+                    } else {
+                        tokio::time::sleep(TRACE_INTERVAL).await;
+                    }
                 }
             })
         });
         Self {
             weighted,
             rtt,
+            loss,
             task_handle,
         }
     }
@@ -140,6 +179,10 @@ where
 
     pub fn rtt(&self) -> Option<Duration> {
         *self.rtt.read().unwrap()
+    }
+
+    pub fn loss(&self) -> Option<f64> {
+        *self.loss.read().unwrap()
     }
 }
 
