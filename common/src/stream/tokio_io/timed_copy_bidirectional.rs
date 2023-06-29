9ee4c09028c110ee4422d87ebf16c88ab86c008e
Banyc <36535895+Banyc@users.noreply.github.com>
2023-06-29 13:36:58 +0800

    fix: stream data corruption

--- common/src/stream/tokio_io/timed_copy_bidirectional.rs
+++ common/src/stream/tokio_io/timed_copy_bidirectional.rs
@@ -6,7 +6,7 @@ use tokio_io_timeout::TimeoutStream;
 
 use crate::tokio_io::ScopedJoinSet;
 
-use super::CopyBiError;
+use super::{copy_bidirectional, CopyBiError};
 
 const UPLINK_TIMEOUT: Duration = Duration::from_secs(60 * 60 * 2);
 const DOWNLINK_TIMEOUT: Duration = Duration::from_secs(60);
@@ -28,46 +28,46 @@ where
     b.set_read_timeout(Some(DOWNLINK_TIMEOUT));
     b.set_write_timeout(Some(UPLINK_TIMEOUT));
 
-    // let a = Box::pin(a);
-    // let b = Box::pin(b);
+    let mut a = Box::pin(a);
+    let mut b = Box::pin(b);
 
-    // copy_bidirectional(&mut a, &mut b).await
+    copy_bidirectional(&mut a, &mut b).await
 
-    let (a_r, mut a_w) = tokio::io::split(a);
-    let (b_r, mut b_w) = tokio::io::split(b);
+    // let (mut a_r, mut a_w) = tokio::io::split(a);
+    // let (mut b_r, mut b_w) = tokio::io::split(b);
 
-    let limiter = <Limiter>::new(speed_limit);
-    let mut a_r = limiter.clone().limit(a_r);
-    let mut b_r = limiter.limit(b_r);
+    // // let limiter = <Limiter>::new(speed_limit);
+    // // let mut a_r = limiter.clone().limit(a_r);
+    // // let mut b_r = limiter.limit(b_r);
 
-    let mut join_set = ScopedJoinSet::new();
-    join_set.spawn(async move {
-        tokio::io::copy(&mut a_r, &mut b_w)
-            .await
-            .map(CopyResult::AToB)
-            .map_err(CopyBiError::FromAToB)
-    });
-    join_set.spawn(async move {
-        tokio::io::copy(&mut b_r, &mut a_w)
-            .await
-            .map(CopyResult::BToA)
-            .map_err(CopyBiError::FromBToA)
-    });
+    // let mut join_set = ScopedJoinSet::new();
+    // join_set.spawn(async move {
+    //     tokio::io::copy(&mut a_r, &mut b_w)
+    //         .await
+    //         .map(CopyResult::AToB)
+    //         .map_err(CopyBiError::FromAToB)
+    // });
+    // join_set.spawn(async move {
+    //     tokio::io::copy(&mut b_r, &mut a_w)
+    //         .await
+    //         .map(CopyResult::BToA)
+    //         .map_err(CopyBiError::FromBToA)
+    // });
 
-    let mut a_to_b = None;
-    let mut b_to_a = None;
-    while let Some(res) = join_set.join_next().await {
-        let res = res.unwrap()?;
-        match res {
-            CopyResult::AToB(n) => {
-                a_to_b = Some(n);
-            }
-            CopyResult::BToA(n) => {
-                b_to_a = Some(n);
-            }
-        }
-    }
-    Ok((a_to_b.unwrap(), b_to_a.unwrap()))
+    // let mut a_to_b = None;
+    // let mut b_to_a = None;
+    // while let Some(res) = join_set.join_next().await {
+    //     let res = res.unwrap()?;
+    //     match res {
+    //         CopyResult::AToB(n) => {
+    //             a_to_b = Some(n);
+    //         }
+    //         CopyResult::BToA(n) => {
+    //             b_to_a = Some(n);
+    //         }
+    //     }
+    // }
+    // Ok((a_to_b.unwrap(), b_to_a.unwrap()))
 }
 
 enum CopyResult {
