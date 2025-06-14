fea998bd98d4af33238df95e7eeab64363fbb638
Banyc <36535895+Banyc@users.noreply.github.com>
2025-06-14 23:04:14 +0800

    chore: bump tokio_chacha20

--- common/src/proto/io_copy/mod.rs
+++ common/src/proto/io_copy/mod.rs
@@ -3,8 +3,8 @@ use std::task::{Context, Poll, Waker};
 use tokio_chacha20::{
     KEY_BYTES, X_NONCE_BYTES,
     stream::{
-        NonceBuf, NonceCiphertextReader, NonceCiphertextReaderConfig, NonceCiphertextTagWriter,
-        NonceCiphertextTagWriterConfig,
+        NonceBuf, NonceCiphertextReader, NonceCiphertextReaderConfig, NonceCiphertextWriter,
+        NonceCiphertextWriterConfig,
     },
 };
 
@@ -15,7 +15,7 @@ pub fn same_key_nonce_ciphertext<R, W>(
     key: &[u8; KEY_BYTES],
     r: R,
     w: W,
-) -> (NonceCiphertextReader<R>, NonceCiphertextTagWriter<W>) {
+) -> (NonceCiphertextReader<R>, NonceCiphertextWriter<W>) {
     let r = nonce_ciphertext_reader(key, r);
     let w = nonce_ciphertext_writer(key, w);
     (r, w)
@@ -25,14 +25,14 @@ fn nonce_ciphertext_reader<R>(key: &[u8; KEY_BYTES], r: R) -> NonceCiphertextRea
     let nonce_buf = NonceBuf::XNonce(Box::new([0; X_NONCE_BYTES]));
     NonceCiphertextReader::new(&reader_config, Box::new(*key), nonce_buf, r)
 }
-fn nonce_ciphertext_writer<W>(key: &[u8; KEY_BYTES], w: W) -> NonceCiphertextTagWriter<W> {
-    let writer_config = NonceCiphertextTagWriterConfig {
+fn nonce_ciphertext_writer<W>(key: &[u8; KEY_BYTES], w: W) -> NonceCiphertextWriter<W> {
+    let writer_config = NonceCiphertextWriterConfig {
         write_nonce: true,
-        write_tag: false,
         key,
+        hash: false,
     };
     let nonce = NonceBuf::XNonce(Box::new(rand::random()));
-    NonceCiphertextTagWriter::new(&writer_config, nonce, w)
+    NonceCiphertextWriter::new(&writer_config, nonce, w)
 }
 
 fn noop_context() -> Context<'static> {
