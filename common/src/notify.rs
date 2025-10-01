cf0c1887fea25695368f8b0edec65721ac16a94c
Banyc <36535895+Banyc@users.noreply.github.com>
2025-10-01 20:00:55 +0800

    refactor: factor out iter set

--- common/src/notify.rs
+++ common/src/notify.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, Mutex, atomic::AtomicUsize};
+use crate::notify::iter_set::{GuardedIterSet, IterSetEntryGuard};
 
 fn binary_event_channel() -> (BinaryEventTx, BinaryEventRx) {
     let (tx, rx) = tokio::sync::mpsc::channel(1);
@@ -9,18 +9,106 @@ struct BinaryEventTx(pub tokio::sync::mpsc::Sender<()>);
 #[derive(Debug)]
 struct BinaryEventRx(pub tokio::sync::mpsc::Receiver<()>);
 
-#[derive(Debug)]
-struct WaiterHandler {
-    pub trigger: BinaryEventTx,
-    pub index: Arc<AtomicUsize>,
+mod iter_set {
+    use std::sync::{Arc, Mutex, atomic::AtomicUsize};
+
+    use derive_more::Debug;
+
+    #[derive(Debug)]
+    pub struct IterSet<T> {
+        buf: Vec<Entry<T>>,
+    }
+    #[derive(Debug)]
+    struct Entry<T> {
+        pub value: T,
+        pub index: Arc<AtomicUsize>,
+    }
+    impl<T> Default for IterSet<T> {
+        fn default() -> Self {
+            Self { buf: vec![] }
+        }
+    }
+    impl<T> IterSet<T> {
+        pub fn values_mut(&mut self) -> impl Iterator<Item = &mut T> {
+            self.buf.iter_mut().map(|entry| &mut entry.value)
+        }
+        pub fn append(&mut self, v: T) -> Arc<AtomicUsize> {
+            let new_index = Arc::new(AtomicUsize::new(self.buf.len()));
+            let waiter_handler = Entry {
+                value: v,
+                index: new_index.clone(),
+            };
+            self.buf.push(waiter_handler);
+            new_index
+        }
+        pub fn remove(&mut self, i: usize) {
+            self.buf.swap_remove(i);
+            let Some(replacer) = self.buf.get(i) else {
+                return;
+            };
+            replacer
+                .index
+                .store(i, std::sync::atomic::Ordering::Relaxed);
+        }
+        pub fn len(&self) -> usize {
+            self.buf.len()
+        }
+    }
+    #[derive(Debug)]
+    #[debug(bound(T:))]
+    pub struct GuardedIterSet<T> {
+        ptr: Arc<Mutex<IterSet<T>>>,
+    }
+    impl<T> Clone for GuardedIterSet<T> {
+        fn clone(&self) -> Self {
+            Self {
+                ptr: self.ptr.clone(),
+            }
+        }
+    }
+    impl<T> Default for GuardedIterSet<T> {
+        fn default() -> Self {
+            let buf = IterSet::default();
+            let ptr = Arc::new(Mutex::new(buf));
+            Self { ptr }
+        }
+    }
+    #[allow(unused)]
+    impl<T> GuardedIterSet<T> {
+        pub fn add(&self, v: T) -> IterSetEntryGuard<T> {
+            let ptr = self.ptr.clone();
+            let mut buf = self.ptr.lock().unwrap();
+            let index = buf.append(v);
+            IterSetEntryGuard { buf: ptr, index }
+        }
+        pub fn values_mut(&self, mut f: impl FnMut(&mut T)) {
+            let mut buf = self.ptr.lock().unwrap();
+            for v in buf.values_mut() {
+                f(v);
+            }
+        }
+        pub fn len(&self) -> usize {
+            self.ptr.lock().unwrap().len()
+        }
+    }
+    #[derive(Debug)]
+    pub struct IterSetEntryGuard<T> {
+        buf: Arc<Mutex<IterSet<T>>>,
+        index: Arc<AtomicUsize>,
+    }
+    impl<T> Drop for IterSetEntryGuard<T> {
+        fn drop(&mut self) {
+            let mut buf = self.buf.lock().unwrap();
+            let i = self.index.load(std::sync::atomic::Ordering::Relaxed);
+            buf.remove(i);
+        }
+    }
 }
-type Waiters = Vec<WaiterHandler>;
 
 #[derive(Debug)]
 pub struct Waiter {
     event: BinaryEventRx,
-    waiter_index: Arc<AtomicUsize>,
-    waiters: Arc<Mutex<Waiters>>,
+    _guard: IterSetEntryGuard<BinaryEventTx>,
 }
 impl Waiter {
     pub fn has_notified(&self) -> bool {
@@ -33,52 +121,30 @@ impl Waiter {
         self.event.0.recv().await.unwrap();
     }
 }
-impl Drop for Waiter {
-    fn drop(&mut self) {
-        let mut waiters = self.waiters.lock().unwrap();
-        let index = self.waiter_index.load(std::sync::atomic::Ordering::Relaxed);
-        waiters.swap_remove(index);
-        let Some(replacer) = waiters.get(index) else {
-            return;
-        };
-        replacer
-            .index
-            .store(index, std::sync::atomic::Ordering::Relaxed);
-    }
-}
 
 #[derive(Debug, Clone)]
 pub struct Notify {
-    waiters: Arc<Mutex<Waiters>>,
+    waiters: GuardedIterSet<BinaryEventTx>,
 }
 impl Notify {
     pub fn new() -> Self {
         Self {
-            waiters: Arc::new(Mutex::new(vec![])),
+            waiters: GuardedIterSet::default(),
         }
     }
     pub fn waiter(&self) -> Waiter {
-        let waiters_ref = self.waiters.clone();
-        let mut waiters = self.waiters.lock().unwrap();
-        let new_index = Arc::new(AtomicUsize::new(waiters.len()));
         let (tx, rx) = binary_event_channel();
-        let waiter_handler = WaiterHandler {
-            trigger: tx,
-            index: new_index.clone(),
-        };
-        waiters.push(waiter_handler);
+        let guard = self.waiters.add(tx);
         Waiter {
             event: rx,
-            waiter_index: new_index.clone(),
-            waiters: waiters_ref.clone(),
+            _guard: guard,
         }
     }
     pub fn notify_waiters(&self) {
-        let mut waiters = self.waiters.lock().unwrap();
-        for waiter in &mut *waiters {
-            assert!(!waiter.trigger.0.is_closed());
-            let _ = waiter.trigger.0.try_send(());
-        }
+        self.waiters.values_mut(|waiter| {
+            assert!(!waiter.0.is_closed());
+            let _ = waiter.0.try_send(());
+        });
     }
 }
 impl Default for Notify {
@@ -105,12 +171,12 @@ mod tests {
         w1.notified().await;
 
         drop(w1);
-        assert_eq!(n.waiters.lock().unwrap().len(), 1);
+        assert_eq!(n.waiters.len(), 1);
 
         n.notify_waiters();
         w2.notified().await;
 
         drop(w2);
-        assert_eq!(n.waiters.lock().unwrap().len(), 0);
+        assert_eq!(n.waiters.len(), 0);
     }
 }
