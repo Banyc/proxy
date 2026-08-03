224a47ce8187edb3f8bc1bd2eb0e80c0452a08fe
Banyc <36535895+Banyc@users.noreply.github.com>
2026-08-03 22:42:03 +0800

    protocol/socks5: use size-assert const to avoid clippy eq_op deny

--- protocol/src/socks5/messages/mod.rs
+++ protocol/src/socks5/messages/mod.rs
@@ -380,7 +380,7 @@ impl RelayResponse {
         W: AsyncWrite + Unpin,
     {
         // VERSION + reply + RSV + ATYP + addr(0xff) + BND.PORT
-        const _: () = assert!(1 + 1 + 1 + 1 + 1 + 0xff + 2 == 262);
+        const _: [(); 262] = [(); 1 + 1 + 1 + 1 + 1 + 0xff + 2];
         let mut buf = [0u8; 1 + 1 + 1 + 1 + 1 + 0xff + 2];
         let mut internal_writer = io::Cursor::new(buf.as_mut_slice());
         internal_writer.write_u8(VERSION).await.unwrap();
